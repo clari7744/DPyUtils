7136523cbbf7afd4136529ea7b0ba46767dfee3a
Clari <clarinet.puppy@gmail.com>
2021-04-28 13:29:37 -0500

    Setting up imports and setup(bot)

---
+++ ContextEditor/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+ContextEditor
+~~~~~~~~~~~~~
+
+A standalone context editor system. This allows the user to edit their message, and the command response will be edited accordingly. Add `bot.load_extension("ContextEditor")` to use this.
+:copyright: (c) 2021-present Clari
+:license: MIT, see LICENSE for more details.
+"""
+
+from context import *
+
+def setup(bot: commands.Bot):
+    Editor(bot)
