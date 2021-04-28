85e66460a40bb27daee0171bdc0b91e8d7aaf4b4
Clari <clarinet.puppy@gmail.com>
2021-04-28 12:56:54 -0500

    Just importing all

---
+++ converters/__init__.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+
+"""
+Converters
+~~~~~~~~~~
+
+Some custom converters to allow for searching instead of exact naming.
+:copyright: (c) 2021-present Clari
+:license: MIT, see LICENSE for more details.
+"""
+
+from converters import *
