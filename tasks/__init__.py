21851312ce41ef2f54bae4ec0e89a7462c10a68b
Clari <clarinet.puppy@gmail.com>
2021-04-28 12:52:12 -0500

    it’s literally just import setup lol

---
+++ tasks/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tasks
+~~~~~
+
+A tasks/to-do list system. Simply load this extension in your main file using `bot.load_extension("tasks")`
+
+:copyright: (c) 2021-present Clari
+:license: MIT, see LICENSE for more details.
+"""
+
+from tasks import setup
