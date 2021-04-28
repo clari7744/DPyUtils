425579521a545ee761028e368fe4ed5c7fd6be7b
Clari <clarinet.puppy@gmail.com>
2021-04-28 13:48:35 -0500

    maybe this’ll work

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dpy-utils",
+    version="0.0.1",
+    author="Clari",
+    author_email="clarinet.puppy@gmail.com",
+    description="Some discord.py utils by Clari",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Clari-7744/dpy-utils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"ContextEditor": "ContextEditor", "converters": "converters", "duration": "duration", "tasks": "tasks"},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
