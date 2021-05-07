129d798a898497d6c2194a90d36bde19e374c65d
Clari <clarinet.puppy@gmail.com>
2021-05-06 22:03:38 -0500

    ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dpy-utils",
-    version="0.0.1",
+    version="0.0.5",
     author="Clari",
     author_email="clarinet.puppy@gmail.com",
     url="https://github.com/Clari-7744/dpy-utils",
