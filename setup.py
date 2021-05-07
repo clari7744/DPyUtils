fa5938cfc44f40435a2083a7469193b8db6b504d
Clari <clarinet.puppy@gmail.com>
2021-05-06 22:13:44 -0500

    trying to be fancy with version

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 from setuptools import setup
+import subprocess
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+ver = "0.0.5"
+
+try:  # shamelessly stolen from jishaku
+    ccount, cerr = subprocess.Popen(
+        ["git", "rev-list", "--count", "HEAD"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ).communicate()
+    if ccount:
+        chash, cerr = subprocess.Popen(
+            ["git", "rev-parse", "--short", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ).communicate()
+        if chash:
+            ver += ccount.decode().strip() + "+" + chash.decode().strip()
+except:
+    pass
+
 setup(
     name="dpy-utils",
-    version="0.0.5",
+    version=ver,
     author="Clari",
     author_email="clarinet.puppy@gmail.com",
     url="https://github.com/Clari-7744/dpy-utils",
