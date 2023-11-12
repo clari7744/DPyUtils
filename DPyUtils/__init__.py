7050e0d88d44aeb4ca62c7fa82bb3bf44cc59306
Clari <clarinet.puppy@gmail.com>
2023-11-12 16:51:17 -0600

    De-wildcard init and format

--- DPyUtils/__init__.py
+++ DPyUtils/__init__.py
@@ -1,10 +1,38 @@
-from .flags import *
-from .checks import *
-from .ContextEditor2 import *
-from .converters import *
-from .duration import Duration, ParsedDuration
+from .checks import check_hierarchy, is_guild_owner
+from .ContextEditor2 import Context, ContextEditor, DeleteButton
+from .converters import (
+    AnyChannel,
+    BotMember,
+    BotUser,
+    CategoryChannel,
+    Color,
+    Emoji,
+    ForumChannel,
+    Guild,
+    GuildChannel,
+    HumanMember,
+    HumanUser,
+    IgnoreCaseLiteral,
+    IntList,
+    InvalidPermission,
+    KillCommand,
+    Member,
+    Message,
+    NewsChannel,
+    NonCategoryChannel,
+    Permissions,
+    Role,
+    StageChannel,
+    TextChannel,
+    Thread,
+    User,
+    UserNotType,
+    VoiceChannel,
+)
+from .duration import Duration, InvalidTimeFormat, ParsedDuration
 from .duration import parse as parse_duration
 from .duration import strfdur
-from .transformers import *
-from .utils import *
-from .views import *
+from .flags import Flag, FlagConverter, FlagIsSwitch, flag, get_flag_signature
+from .transformers import DurationTransformer, IntListTransformer
+from .utils import _and, an, load_extensions, s, trim, try_dm, yn
+from .views import Confirmation
